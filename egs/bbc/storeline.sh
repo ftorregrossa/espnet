3f5b04917c9b7fadd72690a8a52093cc2aaae0ee
Francois Torregrossa <fpt17@sprite30.doc.ic.ac.uk>
2018-08-05 15:34:17 +0100

    working

--- egs/bbc/storeline.sh
+++ egs/bbc/storeline.sh
@@ -6,15 +6,15 @@ basepath=$3
 outfile=$4
 utt2spk=$5
 
-export DIRECTORY=`dirname ${line} | cut -c3-`
+export DIRECTORY=`dirname ${line} | cut -c2-`
 export FILENAME=`basename ${line} .${pattern}`
 export ID=`echo ${DIRECTORY}/${FILENAME} | sed -r 's/[/]+/-/g'`
 
-if [ utt2spk -eq 0 ]; then
+if [ ${utt2spk} -eq 0 ]; then
     if [ "${pattern}" = "txt" ]; then
         export CONTENT="`cat ${line} | grep Text | cut -c8-`"
     else
-        export CONTENT=${basepath}/${DIRECTORY}/${FILENAME}.${pattern}
+        export CONTENT=/${DIRECTORY}/${FILENAME}.${pattern}
     fi
 else
     export CONTENT=`basename ${DIRECTORY}`
